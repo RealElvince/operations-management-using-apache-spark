7f03a586c8322a1a36a9f6e0a27901e3b80a99a8
RealElvince <obuongely@yahoo.com>
2025-07-16 20:13:25 +0300

    transformation to calculate total_price

--- products_spark.py
+++ products_spark.py
@@ -20,3 +20,34 @@ product_data = spark.read.format("csv")\
     .load()
 
 product_data.printSchema()
+product_data.show(10)
+
+# transformations DATAFRAME API
+
+output = product_data.select("product_name", "quantity", "unit_price")\
+        .filter((product_data["quantity"] > 0) & (product_data["unit_price"] > 0))\
+        .withColumn("total_price", func.col("quantity") * func.col("unit_price"))\
+        .orderBy(func.col("total_price").desc())\
+        .cache()
+
+
+output.printSchema()
+
+# create temporary view
+output.createOrReplaceTempView("products_view")
+# run SQL query, SQL API
+# query = """
+#   SELECT product_name, quantity, unit_price, total_price
+#   FROM products_view
+#   ORDER BY total_price DESC
+# """
+# result = spark.sql(query)
+# result.show(10)
+
+# save the output to a new CSV file
+output.write.format("csv")\
+   .mode("overwrite")\
+   .option("header", "true")\
+   .option("path", "data/products_output.csv")\
+   .partitionBy("product_name")\
+   .save()
