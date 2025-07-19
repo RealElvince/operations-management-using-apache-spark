eb777494d06edb8c916884c2f092445f647cd55d
RealElvince <obuongely@yahoo.com>
2025-07-19 16:49:37 +0300

    streaming data from csv files

---
+++ source_sink.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+
+# define input,output and checkpoint directories
+input_dir = "data/input"
+output_dir = "data/output"
+checkpoint_dir = "./checkpoint"
+
+# create Spark session
+spark = SparkSession.builder \
+    .appName("source_sink")\
+    .getOrCreate()
+
+
+
+# define input file schema
+input_schema = StructType([
+    StructField("id", IntegerType(), True),
+    StructField("name", StringType(), True),
+    StructField("age", IntegerType(), True),
+    StructField("friends", IntegerType(), True)
+    ])
